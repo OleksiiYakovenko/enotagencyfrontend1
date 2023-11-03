2be9d9833c4c1cd1cb13335cc29a54e205697e76
Oleksii <yakovenko.alexsei@gmail.com>
2023-11-03 15:38:38 +0200

    df

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar(){
         <nav>
             <div className='navbar'>
                 <div className='logo'>
-                    <img src={require('../images/enotlogo.png' )} alt='loho.png' />
+                    <img src={require('../images/enotlogo.png' )} alt='logo.png' />
                 </div>
                 <div className='c1'>
                     <h4><a href='#buyer'>Покупцям</a></h4>
@@ -19,11 +19,11 @@ export default function Navbar(){
                 <div className='c4'>
                     <h4>+38 (093) 962 08 67</h4>
                     <br/>
-                    <img src={require('../images/faceicon.png' )} alt='pers.png' />
-                    <a href="https://www.instagram.com/enotagancy/"><img src={require('../images/insticon.png')} alt='pers.png' /></a>
-                    <img src={require('../images/tikicon.png' )} alt='pers.png' />
-                    <img src={require('../images/teleicon.png' )} alt='pers.png' />
-                    <img src={require('../images/vibericon.png' )} alt='pers.png' />
+                    <img src={require('../images/faceicon.png' )} alt='facebook.png' />
+                    <a href="https://www.instagram.com/enotagancy/"><img src={require('../images/insticon.png')} alt='instagram.png' /></a>
+                    <img src={require('../images/tikicon.png' )} alt='tiktok.png' />
+                    <img src={require('../images/teleicon.png' )} alt='telegram.png' />
+                    <img src={require('../images/vibericon.png' )} alt='viber.png' />
                 </div>
             </div>
         </nav>)
