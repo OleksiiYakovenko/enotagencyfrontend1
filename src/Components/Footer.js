d74405639169a73094a3484dff212d4647ade8ae
Oleksii <yakovenko.alexsei@gmail.com>
2023-11-03 16:08:13 +0200

    df

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -14,11 +14,11 @@ export default function Footer(){
                         <br/>
                         <h4>+38 (093) 962 08 67</h4>
                         <br/>
-                        <img src={require('../images/teleicon.png')} alt='telegram.png' />
-                        <img src={require('../images/vibericon.png')} alt='viber.png' />
-                        <img src={require('../images/faceicon.png')} alt='facebook.png' />
+                        <img src={require('../images/teleicon.png')} alt='telegram' />
+                        <img src={require('../images/vibericon.png')} alt='viber' />
+                        <img src={require('../images/faceicon.png')} alt='facebook' />
                         <a href="https://www.instagram.com/enotagancy/"><img src={require('../images/insticon.png')} alt='instagram.png' /></a>
-                        <img src={require('../images/tikicon.png')} alt='tiktok.png' />
+                        <img src={require('../images/tikicon.png')} alt='tiktok' />
                     </div>
                     <br/>
                     <br/>
